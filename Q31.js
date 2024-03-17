4cb3242fec6ff5909f18988bbbf0a24af5ebb6a5
Habiba <160347457+Habiba-Wasim@users.noreply.github.com>
2024-03-17 22:49:56 +0500

    Step 31 of Typescript
    Step 31 of Typescript

---
+++ Q31.js
@@ -0,0 +1,22 @@
+"use strict";
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+Object.defineProperty(exports, "__esModule", { value: true });
+// let usernames: string[]=["Bilal", "Kashif", "Fahad", "haiba"];
+// if(usernames.length ===0){
+//     console.log("We need to find some users!")
+// }else{
+//     for(let i=0; i< usernames.length; i++);
+// }
+// console.log(usernames)
+//2nd Method
+let usernames = [];
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    for (let i = 0; i < usernames.length; i++)
+        ;
+}
+console.log(usernames);
---
+++ Q31.ts
@@ -0,0 +1,23 @@
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+
+// let usernames: string[]=["Bilal", "Kashif", "Fahad", "haiba"];
+
+// if(usernames.length ===0){
+//     console.log("We need to find some users!")
+// }else{
+//     for(let i=0; i< usernames.length; i++);
+// }
+// console.log(usernames)
+
+//2nd Method
+
+let usernames: string[]=[];
+
+if(usernames.length ===0){
+    console.log("We need to find some users!")
+}else{
+    for(let i=0; i< usernames.length; i++);
+}
+console.log(usernames)
